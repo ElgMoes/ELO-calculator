0eead4ace40e1541d4f87a19739b3913ccd976f3
ElgMoes <JelleWHoek@gmail.com>
2024-10-17 13:13:18 +0200

    first simple version

---
+++ computation.jl
@@ -0,0 +1,78 @@
+using DataFrames
+using Random
+using Statistics
+using CSV
+using ProgressMeter
+include("setup.jl")
+
+df = create_players(1500)
+
+function average_skill(df)
+    return mean(df.skill)
+end
+
+function create_groups(df, range)
+    group_df = df[range, [:id, :score, :skill, :consistency, :score_history]]
+    shuffle!(group_df)
+    group1_df = group_df[1:5, [:id, :score, :skill, :consistency, :score_history]]
+    group2_df = group_df[6:10, [:id, :score, :skill, :consistency, :score_history]]
+
+    sort!(group1_df, [:skill])
+    sort!(group2_df, [:skill])
+
+    return group1_df, group2_df
+end
+
+function merge_dataframe(df, df_new)
+    for row in eachrow(df_new)
+        idx = findfirst(x -> x == row.id, df.id)
+        if idx !== nothing
+            df.score[idx] = row.score
+            push!(df.score_history[idx], row.score)
+        end
+    end
+end
+
+function play_game(df, range)
+    group1_df, group2_df = create_groups(df, range)
+
+    skill1 = average_skill(group1_df)
+    skill2 = average_skill(group2_df)
+
+    E1 = 1/(1+10^((skill2-skill1)/400))
+    E2 = 1/(1+10^((skill1-skill2)/400))
+
+    K = 32
+
+    if skill1 > skill2
+        group1_df.score .+= round.(K*(1-E1))
+        group2_df.score .+= round.(K*(0-E2))
+    elseif skill1 < skill2
+        group1_df.score .+= round.(K*(0-E1))
+        group2_df.score .+= round.(K*(1-E2))
+    end
+
+    merge_dataframe(df, group1_df)
+    merge_dataframe(df, group2_df)
+    return df
+end
+
+function play_round(df)
+    for i in 1:(nrow(df)/10)
+        range = Int(round(10*i - 9)):Int(round(10*i))
+        df = play_game(df, range)
+    end
+    sort!(df, :score)
+    return df
+end
+
+function simulate(df, rounds)
+    @showprogress for i in 1:rounds
+        df = play_round(df)
+    end
+    return df
+end
+
+df = simulate(df, 100)
+
+CSV.write("out.csv",df)
---
+++ setup.jl
@@ -0,0 +1,36 @@
+using DataFrames
+using Sqids
+
+function create_players(players)
+    function random_new_hex(player)
+        config = Sqids.configure(minLength=10)
+        id = Sqids.encode(config, [player, player + 1])
+        return id
+    end
+    
+    function gaussian_bell_curve(x, μ, σ)
+        return exp(-((x - μ)^2) / (2 * σ^2))*players/600
+    end
+
+    μ = players/200.0
+    σ = players/1000
+
+    df = DataFrame(
+        id=String[],
+        score=UInt16[],
+        skill=Float64[],
+        consistency=Float64[],
+        score_history=Vector{UInt16}[]
+    );
+
+    for i in 1:players
+        id = random_new_hex(i)
+        score = 1000
+        skill = i / 100.0
+        consistency = gaussian_bell_curve(skill, μ, σ)
+        score_history = [score]
+
+        push!(df, (id, score, skill, consistency, score_history))
+    end
+    return df
+end
